030ca472a7643cab99beb49f2a6ada10769087cd
LSnyd <lisa@luethen-schneider.de>
2021-02-10 13:04:16 +0100

    debug: path

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -25,7 +25,7 @@ from tensorflow.python.eager import context
 import tensorflow.keras.models as KM
 
 # Root directory of the project
-ROOT_DIR = os.path.abspath("../")
+ROOT_DIR = os.path.abspath("./")
 
 # Import Mask RCNN
 sys.path.append(ROOT_DIR)  # To find local version of the library
